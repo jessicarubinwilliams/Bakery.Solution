ed775d274c020261ad63f70c350a809c79f9da12
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-19 11:38:11 -0400

    Add code for passing test of GetNumPastries_ReturnsNumPastries_Int()

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,11 @@ namespace Bakery.Models
 {
   public class Pastry
   {
-    
+    public int NumPastries { get; }
+
+    public Pastry(int pastries)
+    {
+      NumPastries = pastries;
+    }
   }
 }
