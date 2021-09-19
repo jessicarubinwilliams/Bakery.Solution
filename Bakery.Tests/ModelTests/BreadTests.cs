a7d0e00fc3b95626d4e8f8ed9ce28e091157200d
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-19 11:47:08 -0400

    Add passing test for BreadConstructor_CreatesInstanceOfBread_Bread()

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -6,6 +6,11 @@ namespace Bakery.Tests
   [TestClass]
   public class BreadTests
   {
-    
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
   }
 }
