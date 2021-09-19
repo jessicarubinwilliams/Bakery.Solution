249ba04e28b821543498bc0641ff3a00844f849a
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-09-19 11:49:09 -0400

    Add fail of test GetNumLoaves_ReturnsNumLoaves_Int()

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,8 +9,17 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(5);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetNumLoaves_ReturnsNumLoaves_Int()
+    {
+      int numLoaves = 5;
+      Bread newBread = new Bread(numLoaves);
+      int result = newBread.NumLoaves;
+      Assert.AreEqual(numLoaves, result);
     }
   }
 }
